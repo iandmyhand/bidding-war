24d7e92976fa57d56842bd2168f6fec237266d1e
symnoisy <symnoisy@gmail.com>
2021-07-18 20:55:44 +0900

    - ORM 알아보는중

---
+++ src/test/kotlin/com/example/bidding_war/BiddingWarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bidding_war
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BiddingWarApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
