a54904b0121f952c8cf05897998af6fbef392794
symnoisy <symnoisy@gmail.com>
2021-07-18 14:30:25 +0900

    init

---
+++ src/main/kotlin/com/example/jungbeen_bidding_war/JungbeenBiddingWarApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jungbeen_bidding_war
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JungbeenBiddingWarApplication
+
+fun main(args: Array<String>) {
+    runApplication<JungbeenBiddingWarApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/jungbeen_bidding_war/JungbeenBiddingWarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jungbeen_bidding_war
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JungbeenBiddingWarApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
