0cdf041feb37e5e0f2673338cc9f52dc76a65d41
SeomGi, Han <iandmyhand@gmail.com>
2021-07-20 19:11:28 +0900

    Initial commit

---
+++ src/main/kotlin/kr/co/peoplefund/biddingWar/BiddingWarApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.peoplefund.biddingWar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BiddingWarApplication
+
+fun main(args: Array<String>) {
+	runApplication<BiddingWarApplication>(*args)
+}
