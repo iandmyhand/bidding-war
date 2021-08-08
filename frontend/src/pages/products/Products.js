46d165654bbccc213f29a9b74208c6db86a3e144
Sung Jun Jin <sungjun@peoplefund.co.kr>
2021-08-08 21:09:41 +0900

    feat: 기존 product 컴포넌트 스타일 개선

--- frontend/src/pages/products/Products.js
+++ frontend/src/pages/products/Products.js
@@ -1,11 +1,45 @@
 import React, {createRef, useEffect, useState} from 'react';
 import {createProduct, fetchProducts} from "../../api";
+import styled from "styled-components";
 
 const Products = () => {
     const [products, setProducts] = useState([])
     const inputName = createRef()
     const inputPrice = createRef()
     const inputDescription = createRef()
+
+    const Container = styled.div`
+      margin-top: 100px;
+      padding: 20px;
+      text-align: center;
+    `;
+
+    const Button = styled.div`
+      font-size: 18px;
+      font-weight: 700;
+      line-height: 49px;
+      display: block;
+      width: 10%;
+      height: 5%;
+      margin-left: 45%;
+      cursor: pointer;
+      text-align: center;
+      color: #fff;
+      border-radius: 0;
+      background-color: #03c75a;
+    `;
+
+    const Input = styled.input`
+      position: relative;
+      overflow: hidden;
+      width: 20%;
+      height: 40px;
+      margin: 0 0 8px;
+      padding: 5px 39px 5px 11px;
+      border: solid 1px #dadada;
+      background: #fff;
+      box-sizing: border-box;
+    `;
 
     const initProducts = async () => {
         const response = await fetchProducts()
@@ -35,27 +69,28 @@ const Products = () => {
     useEffect(() => {
     }, [products])
 
-    return <div>
+    return <Container>
         <h1>Bidding-War</h1>
-        {products.map(product => <div key={product.id}>{product.id} {product.name} {product.price} {product.description}}</div>)}
+        {products.map(product => <div key={product.id}>{product.id}. {product.name} {product.price} {product.description}&nbsp;</div>)}
 
         <h2>상품 등록하기</h2>
         <div>
-            <h3>상품명 : </h3>
-            <input type="text" ref={inputName}/>
+            <h3>상품명</h3>
+            <Input type="text" ref={inputName} placeholder="상품명을 입력해주세요"/>
         </div>
         <div>
-            <h3>상품 가격 : </h3>
-            <input type="number" ref={inputPrice}/>
+            <h3>상품 가격</h3>
+            <Input type="number" ref={inputPrice} placeholder="상품 가격을 입력해주세요"/>
         </div>
         <div>
-            <h3>상품 설명 : </h3>
-            <input type="text" ref={inputDescription}/>
+            <h3>상품 설명</h3>
+            <Input type="text" ref={inputDescription} placeholder="상품 설명을 입력해주세요"/>
         </div>
+        &nbsp;
         <div>
-            <button onClick={addProduct}>상품 등록</button>
+            <Button onClick={addProduct}>상품 등록</Button>
         </div>
-    </div>
+    </Container>
 }
 
 export default Products
