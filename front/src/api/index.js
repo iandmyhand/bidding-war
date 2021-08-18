c7ab8ff7e13c886dc4db8138d73e8b075c8049e2
symnoisy <symnoisy@gmail.com>
2021-08-19 00:52:30 +0900

    front 복사

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -5,8 +5,16 @@ const client = axios.create({
     baseURL: `http://localhost:8080`
 })
 
+export const getToken = (data) => client.post('/api/users/sign-in', data)
+
+export const createUser = (data) => client.post('/api/users', data)
+
 export const fetchProducts = () => client.get('/api/products')
 
 export const fetchProduct = (id) => client.get(`/api/products/${id}`)
 
-export const createProduct = (data) => client.post('/api/products', data)
+export const createProduct = (data, token) => client.post('/api/products', data, {
+    "headers": {
+        "Authorization": `${token}`
+    }
+})
