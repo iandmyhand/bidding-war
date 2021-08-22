1e625b1cf0fea9e1ab78d598fbfb13dde92d70b2
Sung Jun Jin <sungjun@peoplefund.co.kr>
2021-08-22 22:11:45 +0900

    feat: 클라이언트 라우팅 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import Products from "./pages/products/Products";
 import Login from "./pages/users/Login";
 
 const App = () => {
   return (
     <div className="App">
-      <Login/>
+        <BrowserRouter>
+          <Route exact path='/products' component={Products} />
+          <Route exact path='/' component={Login} />
+        </BrowserRouter>
     </div>
   );
 }
