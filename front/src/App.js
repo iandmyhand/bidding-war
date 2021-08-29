95b801995637ca4be90ea0f7b38f31b3cd27df28
symnoisy <symnoisy@gmail.com>
2021-08-30 02:04:37 +0900

    - 섬기님 6주차 과제까지는 팔로우업 완료

--- front/src/App.js
+++ front/src/App.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import axios from 'axios';
-import {useEffect, useState} from "react";
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>경매 물품</h1>
-        <ItemList />
-    </div>
-  );
-}
-
-function ItemList() {
-    const [items, setItems] = useState([])
-
-    useEffect(() => {
-        axios.get("/item").then(response => setItems(response.data))
-    }, [])
-
-    return (
-        <div>{
-            items.map(item => (
-                <ul>물품명: { item.title } / 시작가: { item.initialPrice} / 설명: { item.description } / 등록일시: { item.registered }</ul>
-            ))
-        }
-        </div>
-    )
-}
-=======
 import React, { useState } from 'react';
 import Products from "./Products";
 import {Login, LoggedInMessage, RegisterUser} from "./Auth";
@@ -58,4 +28,3 @@ const App = () => {
 }
 
 export default App;
->>>>>>> origin/feature/seomgi/6-session
