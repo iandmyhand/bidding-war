fe82b90ffc2b219795ba20355e99707504bb0913
myeongjae <myeongjae@peoplefund.co.kr>
2021-08-30 02:01:21 +0900

    fix: 설정 수정

--- src/main/kotlin/com/study/biddingwar/config/WebMvcConfig.kt
+++ src/main/kotlin/com/study/biddingwar/config/WebMvcConfig.kt
@@ -1,4 +1,10 @@
 package com.study.biddingwar.config
 
-class WebMvcConfig {
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebMvcConfig: WebMvcConfigurer {
 }
--- src/main/kotlin/com/study/biddingwar/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/study/biddingwar/config/security/SecurityConfig.kt
@@ -1,8 +1,15 @@
 package com.study.biddingwar.config.security
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.*
+import java.util.stream.Collectors
 
 @Configuration
 class SecurityConfig: WebSecurityConfigurerAdapter() {
@@ -18,9 +25,22 @@ class SecurityConfig: WebSecurityConfigurerAdapter() {
             .disable()
             .csrf()
             .disable()
-            .cors()
-            .disable()
+            .cors().and()
             .httpBasic()
             .disable()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = Arrays.asList("*")
+        configuration.allowedMethods =
+            Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+        configuration.allowedHeaders = Arrays.asList("*")
+        configuration.allowCredentials = false
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
