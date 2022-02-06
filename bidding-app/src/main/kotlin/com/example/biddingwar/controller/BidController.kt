9cebcb25ed9c06dcd06ec38fbf6c8a2817f09cad
Sung Jun Jin <sungjun@peoplefund.co.kr>
2022-02-06 22:16:10 +0900

    feat: controller별 swagger 적용

--- bidding-app/src/main/kotlin/com/example/biddingwar/controller/BidController.kt
+++ bidding-app/src/main/kotlin/com/example/biddingwar/controller/BidController.kt
@@ -2,22 +2,29 @@ package com.example.biddingwar.controller
 
 import com.example.biddingwar.database.Bid
 import com.example.biddingwar.service.bid.BidService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
 
+@Api(description = "입찰")
 @RestController
 @RequestMapping("/bids")
 class BidController(val service: BidService) {
     @GetMapping
+    @ApiOperation(value = "입찰목록 확인", notes = "입찰목록 GET API")
     fun bids() = service.getAll()
 
     @GetMapping("/{userId}")
+    @ApiOperation(value = "사용자 입찰목록 확인", notes = "사용자 입찰목록 확인 GET API")
     fun userBids(@PathVariable userId: Long) = ResponseEntity.ok(service.getBidByUserId(userId))
 
     @GetMapping("/{productId}")
+    @ApiOperation(value = "상품 입찰목록 확인", notes = "상품별 입찰목록 확인 GET API")
     fun productBids(@PathVariable productId: Long) = ResponseEntity.ok(service.getByProductId(productId))
 
     @PostMapping
+    @ApiOperation(value = "입찰등록", notes = "입찰등록 POST API")
     fun create(@RequestBody bid: Bid, request: HttpServletRequest): ResponseEntity<Bid> = service.saveBid(bid, request)
 }
--- bidding-app/src/main/kotlin/com/example/biddingwar/controller/UserController.kt
+++ bidding-app/src/main/kotlin/com/example/biddingwar/controller/UserController.kt
@@ -2,22 +2,28 @@ package com.example.biddingwar.controller
 
 import com.example.biddingwar.database.User
 import com.example.biddingwar.service.user.UserService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
 
+@Api(description = "유저")
 @RestController
 @RequestMapping("")
 class UserController(val service: UserService) {
     @GetMapping("/users")
+    @ApiOperation(value = "유저 조회", notes = "유저 조회 GET API")
     fun users() = ResponseEntity.ok(service.getAll())
 
     @PostMapping("/sign-up")
+    @ApiOperation(value = "회원가입", notes = "회원가입 POST API")
     fun signUp(@RequestBody user: User) : ResponseEntity<String> {
         return service.signUp(user)
     }
 
     @PostMapping("/sign-in")
+    @ApiOperation(value = "로그인", notes = "로그인 POST API")
     fun signIn(@RequestBody user: User, request: HttpServletRequest): ResponseEntity<String> {
         return service.signIn(user, request)
     }
